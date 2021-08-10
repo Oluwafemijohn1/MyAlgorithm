c78e757035ec2b66e5d52c0cfa293f0296cb6eaa
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-10 12:06:43 +0100

    changed file to .kt

---
+++ app/src/main/java/com/example/algorithm/LearntSomethingAboutClass.kt
@@ -0,0 +1,24 @@
+package com.example.algorithm;
+
+
+class LearntSomethingAboutClass : Class1() {
+    override fun method() {
+        super.method()
+    }
+}
+
+open class Class1() {
+    open fun method() {
+        print(("Hello"))
+    }
+}
+
+abstract class Class3 {
+    abstract fun anything()
+}
+
+class Class4 : Class3() {
+    override fun anything() {
+    }
+}
+
