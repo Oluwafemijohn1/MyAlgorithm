0b900f514e270bbe0751e2be443e899b457c6d72
Oluwafemijohn <femiogundipe01@gmail.com>
2021-11-23 09:07:03 +0100

    Did re-arangment

---
+++ app/src/main/java/com/example/algorithm/algo/ConvertStringToCamelCase.kt
@@ -0,0 +1,25 @@
+package com.example.algorithm.algo
+
+fun main(args: Array<String>) {
+    println(toCamelCase3("the-stealth-warrior"))
+//    println(toCamelCase3("The_Stealth_Warrior"))
+//    println(toCamelCase3("The-Stealth-Warrior"))
+//    println(toCamelCase3("A-B-C"))
+}
+
+fun toCamelCase(str: String): String = str.split("-", "_").mapIndexed { index, s -> if (index == 0) s else s.capitalize() }.joinToString("")
+
+fun toCamelCase2(str:String):String = str.split(Regex("_|-")).reduce{ fullString, word -> fullString + word.capitalize() }
+
+fun toCamelCase3(s: String) = s.split("-", "_").run {
+    println(this)
+    drop(1).joinToString("", first()) { it.capitalize() }
+}
+
+fun toCamelCase4(str:String):String{
+    var srt = ""
+    return str.split("_", "-").reduce{ fullString, word ->
+//        println(word.capitalize())
+        println(fullString)
+        fullString + word.capitalize() }
+}
---
+++ app/src/main/java/com/example/algorithm/algo/VowelCount.kt
@@ -0,0 +1,32 @@
+package com.example.algorithm.algo
+
+import java.util.regex.Pattern
+
+fun main() {
+    println(getCount("abracadabra"))
+}
+
+fun getCount(str : String) : Int {
+    var count = 0
+    for(i in 0 until str.length){
+        if (Pattern.matches("[aeiou]", str[i].toString())){
+            count++
+        }
+    }
+    return count
+}
+
+fun getCount2(str : String) = str.count { it in "aeiou" }
+fun getCount3(str : String): Int = str.count { it in listOf('a','i','e','o','u') }
+
+fun getCount4(str : String) : Int {
+    return str.count{x->"aeiou".contains(x)}
+}
+
+fun getCount5(str : String) : Int {
+    val regex = Regex("([aeiou])")
+    return regex.findAll(str).count()
+}
+
+fun getCount6(str : String) : Int = str.length - str.replace("[aeiou]".toRegex(),"").length
+fun getCount7(str : String) = str.filter { ch -> ch in "aeiou" }.length
